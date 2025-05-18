05166aa8a74b7a16f6dba479c9fcb21c0186494d
hprogram <wjs2096@gmail.com>
2025-05-19 04:36:38 +0900

    feat(gateway): RolesGuard, AuthGuard 적용

--- gateway/src/iam/iam.controller.ts
+++ gateway/src/iam/iam.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Param, Patch, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { IamService } from './iam.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('iam')
 export class IamController {
@@ -20,7 +31,9 @@ export class IamController {
     return this.iamService.login(body);
   }
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Patch('user/:id')
+  @Roles('USER', 'ADMIN')
   updateUser(
     @Param('id') id: string,
     @Body() body: UpdateUserDto,
@@ -29,7 +42,9 @@ export class IamController {
     return this.iamService.updateUser(id, req.headers.authorization, body);
   }
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Patch('user/:id/role')
+  @Roles('ADMIN')
   updateRole(
     @Param('id') id: string,
     @Body() body: UpdateRoleDto,
