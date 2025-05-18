3937d7a81b3f96d14f025b01bde485b0fc64f325
hprogram <wjs2096@gmail.com>
2025-05-19 01:12:20 +0900

    feat(gateway): Roles Guard 및 Decorator 구현

--- gateway/src/auth/auth.module.ts
+++ gateway/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [PassportModule],
-  providers: [JwtStrategy],
-  exports: [PassportModule],
+  providers: [JwtStrategy, RolesGuard],
+  exports: [PassportModule, RolesGuard],
 })
 export class AuthModule {}
