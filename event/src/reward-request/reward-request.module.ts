c2a5d0cd4e7cfb6f32ecfe402c02ea8d965a7c6b
hprogram <wjs2096@gmail.com>
2025-05-19 21:56:35 +0900

    chore(reward-request): Event 모듈 주입 에러 수정

--- event/src/reward-request/reward-request.module.ts
+++ event/src/reward-request/reward-request.module.ts
@@ -3,12 +3,14 @@ import { RewardRequestService } from './reward-request.service';
 import { RewardRequestController } from './reward-request.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RewardRequestSchema } from './schemas/reward-request.schema';
+import { EventModule } from 'src/event/event.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'RewardRequest', schema: RewardRequestSchema },
     ]),
+    EventModule,
   ],
   providers: [RewardRequestService],
   controllers: [RewardRequestController],
